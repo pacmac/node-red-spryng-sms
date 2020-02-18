38e9ef8e3c335762660b8887e1dce6ea54727b45
pacmac <home@peter-c.net>
2020-02-18 08:17:05 +0800

    update

--- index.js
+++ index.js
@@ -1,70 +1,60 @@
 module.exports = function(RED) {
 
+  var debug = false;
+
   var request = require('request');
   const cl = console.log;
+  
   function spryngMain(config) {
 
-    this.atomDate = function(date){
-      // '2020-01-17T19:34:18-08:00'
+    function atomDate(date){
+      
       function pad(val){
-        return val.toString().padStart(2,'0')
+        return val.toString().padStart(2,'0');
       }
+      
       date = date || new Date();
       var raw = (date.getTimezoneOffset()/60).toString();
       var sign = raw.slice(0,1);
-      //sign = '+';
       var osh = pad(raw.slice(1));
       var osm = pad((date.getTimezoneOffset()%60).toString());
       var os = `${sign}${osh}:${osm}`
-      os = '+00:00';
-      return `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDate())}T${pad(date.getHours()-7)}:${pad(date.getMinutes())}:${pad(date.getSeconds())}${os}`;
+      
+      /* 
+          '2020-01-17T19:34:18-08:00'
+          Hack - the server does not use the timezone offset (-08:00)
+          so hard code it and adjust into the hours.
+          problem is daylight saving in NL
+          Also Need another fix to allow for this, probably send bad request to get TZ-oset.
+      */
+      
+      /* HACK START */
+      sos = 1; // server TZ Offset 1 hour
+      osh = date.getTimezoneOffset() / 60 + sos;
+      date.setHours(date.getHours() - osh)
+      date = new Date(date);
+      os = '+01:00';  // Use Fixed NL Offset (needs lookup fix)
+      /* HACK END */
+      
+      var atom = `${date.getFullYear()}-${pad(date.getMonth()+1)}-${pad(date.getDate())}T${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}${os}`
+      cl(`@@ scheduled_at = atomDate(${atom})`);
+      return atom;
     } 
 
-    console.log('@@ SPRYNG-SMS CONFIG:')
-    for(var key in config){
-      this[key] = config[key];
-      console.log(key,config[key]);
-    }
-
-    function send(node,msg){
-    
-      msg.payload.recipients = msg.payload.recipients.split(',');
-      
+    function balance(node,msg){
+      var request = require('request');
       var options = {
-        'method': 'POST',
-        'url': 'https://rest.spryngsms.com/v1/messages',
-        'headers': {
+        'method'          : 'GET',
+        'url'             : 'https://rest.spryngsms.com/v1/balance',
+        'headers'         : {
           'Accept'        : 'application/json',
-          'Authorization' : token,
-          'Content-Type'  : 'application/json'
-        },
-        
-        body: JSON.stringify({
-          "body"          : msg.payload.body,
-          "encoding"      : msg.payload.encoding || node.encoding,
-          "originator"    : msg.payload.originator || node.originator,
-          "recipients"    : msg.payload.recipients,
-          "route"         : msg.payload.route || node.route,
-          "scheduled_at"  : node.atomDate(msg.payload.scheduled_at) || node.atomDate()
-        })
-        
+          'Authorization' : token
+        }
       };
-      
-      cl(options.body);
       
       request(options, function (error, response) { 
         if (error) throw new Error(error);
-        console.log(response.body);
-      
-        /*
-          {"message":"The given data was invalid.","errors":{"credits":["User has insufficient credits; Currently: 0.0 Needed: 1.2"]}}
-          
-          The scheduled at does not match the format Y-m-d\TH:i:sP.
-          
-          '2020-02-17T10:57:55.901Z'
-          2020-01-01T15:00:00+00:00
-        
-        */
+        if(debug) cl(response.body);
         
         node.send({
           payload : {
@@ -75,19 +65,81 @@ module.exports = function(RED) {
           topic   : msg.payload,
           _msgid  : new Date().getTime()
           
+        });      
+      
+      });
+    }
+
+
+    function send(node,msg){
+    
+      msg.payload.recipients = msg.payload.recipients.split(',');
+      
+      var options = {
+        'method'          : 'POST',
+        'url'             : 'https://rest.spryngsms.com/v1/messages',
+        'headers'         : {
+          'Accept'        : 'application/json',
+          'Authorization' : token,
+          'Content-Type'  : 'application/json'
+        },
+        
+        body: JSON.stringify({
+          "body"          : msg.payload.body || '',
+          "encoding"      : msg.payload.encoding || node.encoding,
+          "originator"    : msg.payload.originator || node.originator,
+          "recipients"    : msg.payload.recipients || [],
+          "route"         : msg.payload.route || node.route,
+          "scheduled_at"  : node.atomDate(msg.payload.scheduled_at) || node.atomDate()
+        })
+        
+      };
+      
+      if(debug) cl(options.body);
+      
+      request(options, function (error, response) { 
+        if (error) throw new Error(error);
+        if(debug) console.log(response.body);
+      
+        /*
+          {"message":"The given data was invalid.","errors":{"credits":["User has insufficient credits; Currently: 0.0 Needed: 1.2"]}}
+          
+          The scheduled at does not match the format Y-m-d\TH:i:sP.
+          
+          2020-01-01T15:00:00+00:00
+        
+        */
+        
+        node.send({
+          payload : {
+            body        : JSON.parse(response.body)
+          },
+          
+          topic   : 'balance',
+          _msgid  : new Date().getTime()
+          
         });
       
       });
     }    
     
+    // ### MAIN
     var node = this;
+    
+    if(debug)  console.log('@@ SPRYNG-SMS CONFIG:\n',JSON.stringify(config,null,2));
+    for(var key in config){
+      this[key] = config[key];
+    }
+    
     //if(this.token) const token = `Bearer ${this.token}`;
     const token = `Bearer ${process.env.spryng_token}`;
     
     RED.nodes.createNode(node,config);
     node.on('input', function(msg) {
+      if(msg.payload.debug) debug = true;
       var action = msg.topic;
-      send(this,msg);
+      if(action=='send') send(this,msg);
+      else if(action=='balance') balance(this,msg);
     });
   
   }
